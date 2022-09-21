f7e3b4e42abe8c5ec9c19974a3bc02c12efd28c0
Akhil Padmanabhan <speaktoakhilp@gmail.com>
2022-09-22 00:04:09 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import 'chart.js';
+import 'chartjs-plugin-dragdata';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'forecast-graph';
+  public lineChartData: Array<any> | undefined;
+
+
+  public lineChartOptions: any;
+
+  public lineChartLabels: Array<any> = [100, 200, 300, 400];
+
+  ngOnInit(): void {
+    this.lineChartOptions = {
+      animation: false,
+      responsive: false,
+      title: {
+        display: true,
+        fontSize: 30,
+        text: ''
+      },
+
+      dragData: true,
+      dragX: false,
+      dragDataRound: 10,
+      onDragStart: (event: any, element: any) => { console.log(` `); },
+      onDrag: (event: any, datasetIndex: any, index: any, value: { y: any; }) => { console.log(`New y-value is ${value.y} `); },
+      onDragEnd: (event: any, datasetIndex: any, index: any, value: { y: any; }) => {
+        console.log(`New y-value is ${value.y} `);
+      },
+    };
+
+    this.lineChartData = [
+      {
+        data: [],
+        label: '',
+        yAxisID: 'A',
+        pointHitRadius: 30,
+        fill: false,
+      },
+      {
+        data: [],
+        label: '',
+        yAxisID: 'A',
+        pointHitRadius: 30,
+        fill: false,
+      },
+      {
+        data: [],
+        label: '',
+        yAxisID: 'B',
+        pointHitRadius: 30,
+        fill: false,
+      }
+    ];
+
+
+    for (let i = 0; i < 4; i++) {
+      this.lineChartData[0].data.push({ x: (i + 1) * 100, y: Math.floor((Math.random() * 90) + 10) });
+      this.lineChartData[1].data.push({ x: (i + 1) * 100, y: Math.floor((Math.random() * 80) + 0) });
+      this.lineChartData[2].data.push({ x: (i + 1) * 100, y: Math.floor((Math.random() * 70) + 20) });
+    }
+
+
+  }
 }
